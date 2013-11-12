0792800aa212c66c891f30486607a751e31676cc
lpuiling <lpuiling@umich.edu>
2013-11-11 22:05:58 -0500

    first try

---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QWidget>
+#include <QColor>
+
+namespace Ui {
+class ColorSelect;
+}
+
+class ColorSelect : public QWidget
+{
+    Q_OBJECT
+    Q_PROPERTY(QColor color READ color NOTIFY colorChanged)
+public:
+    explicit ColorSelect(QWidget *parent = 0);
+    ~ColorSelect();
+
+    QColor color() const
+    {
+       return m_color;
+    }
+signals:
+    void colorChanged(QColor arg);
+
+private slots:
+    void onColorChanged();
+
+private:
+    Ui::ColorSelect *ui;
+    QColor m_color;
+};
+
+#endif // MAINWINDOW_H
